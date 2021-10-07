90eee0269dbfb23a0a285ac8d328f82e7f2a416b
gauravthorath <gauravjobs25@gmail.com>
2021-10-07 13:43:40 +0530

    Added grid to show list of members already exist in the list

--- memberportal/src/App.tsx
+++ memberportal/src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [members, setMembers] = useState([
+    {
+      firstName: "Rawadi",
+      lastName: "Rathod",
+      salary: "4000",
+    },
+    {
+      firstName: "Baburao",
+      lastName: "Singham",
+      salary: "40000",
+    },
+  ]);
+
+  function handleClick() {
+    console.log("Add button click");
+    //   // alert("Add button click");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">Member List</header>
+        <div className="App-body">
+          <button type="button" className="btnPrimary" onClick={handleClick}>
+            Add Member
+          </button>
+          <table>
+            <thead>
+              <td>First Name</td>
+              <td>Last Name</td>
+              <td>Salary</td>
+              <td>View</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </thead>
+            <tbody>
+              {members.map((item, index) => {
+                return (
+                  <tr>
+                    <td>{item.firstName} </td>
+                    <td>{item.lastName}</td>
+                    <td> {item.salary}</td>
+                    <td>
+                      <a href="#" className="App-link">
+                        View
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" className="App-link">
+                        Edit
+                      </a>
+                    </td>
+                    <td>
+                      <a href="#" className="App-link">
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
